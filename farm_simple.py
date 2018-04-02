a21a4492497823686961f318450205e9b1643f7f
Sidney Mau <smau@des60.fnal.gov>
2018-04-02 13:44:22 -0500

    Beginning implementation of modal search

--- farm_simple.py
+++ farm_simple.py
@@ -9,8 +9,9 @@ import time
 import subprocess
 import glob
 import pyfits
-import healpy
-import numpy
+import healpy as hp
+import numpy as np
+import fitsio as fits
 
 import ugali.utils.healpix
 
@@ -27,6 +28,8 @@ with open('config.yaml', 'r') as ymlfile:
     nside = cfg[survey]['nside']
     datadir = cfg[survey]['datadir']
     mode = cfg[survey]['mode']
+    sim_catalog = cfg[survey]['sim_catalog']
+    sim_population = cfg[survey]['sim_population']
 
 results_dir = os.path.join(os.getcwd(), cfg['output']['results_dir'])
 if not os.path.exists(results_dir):
@@ -36,29 +39,56 @@ log_dir = os.path.join(os.getcwd(), cfg['output']['results_dir'], cfg['output'][
 if not os.path.exists(log_dir):
     os.mkdir(log_dir)
 
-infiles = glob.glob ('{}/*.fits'.format(datadir))
-
 ############################################################
 
-print('Pixelizing...')
-pix_nside = [] # Equatorial coordinates, RING ordering scheme
-for infile in infiles:
-    pix_nside.append(int(infile.split('.fits')[0].split('_')[-1]))
-
-############################################################
-
-print('mode: {}...'.format(mode))
-
-for ii in range(0, len(pix_nside)):
-#for ii in range(5):
-    ra, dec = ugali.utils.healpix.pixToAng(nside, pix_nside[ii])
-
-    print('({}/{})').format(ii, len(pix_nside))
-
-    logfile = '{}/results_nside_{}_{}.log'.format(log_dir, nside, pix_nside[ii])
+def submitJob(ra, dec, pix):
+    logfile = '{}/results_nside_{}_{}.log'.format(log_dir, nside, pix)
     batch = 'csub -n {} -o {} '.format(jobs, logfile)
     command = 'python {}/search_algorithm.py {:0.2f} {:0.2f}'.format(simple_dir, ra, dec)
     command_queue = batch + command
     print(command_queue)
     #os.system('./' + command) # Run locally
     os.system(command_queue) # Submit to queue
+
+    return
+
+############################################################
+
+print('mode: {}...'.format(mode))
+
+if (mode == 0): # real
+    infiles = glob.glob ('{}/*.fits'.format(datadir))
+    
+    print('Pixelizing...')
+    pix_nside = [] # Equatorial coordinates, RING ordering scheme
+    for infile in infiles:
+        pix_nside.append(int(infile.split('.fits')[0].split('_')[-1]))
+
+    for ii in range(0, len(pix_nside)):
+    #for ii in range(5):
+        ra, dec = ugali.utils.healpix.pixToAng(nside, pix_nside[ii])
+    
+        submitJob(ra, dec, pix_nside[ii])
+        #print('({}/{})').format(ii, len(pix_nside))
+    
+        #logfile = '{}/results_nside_{}_{}.log'.format(log_dir, nside, pix_nside[ii])
+        #batch = 'csub -n {} -o {} '.format(jobs, logfile)
+        #command = 'python {}/search_algorithm.py {:0.2f} {:0.2f}'.format(simple_dir, ra, dec)
+        #command_queue = batch + command
+        #print(command_queue)
+        ##os.system('./' + command) # Run locally
+        #os.system(command_queue) # Submit to queue
+elif (mode == 1): # sim
+    sim_pop = fits.read(sim_population)
+    for sim in sim_pop:
+        ra, dec = sim['ra'], sim['dec']
+        pix = hp.ang2pix(nside, ra, dec, lonlat=True)
+
+        submitJob(ra, dec, pix)
+        #logfile = '{}/results_nside_{}_{}.log'.format(log_dir, nside, pix)
+        #batch = 'csub -n {} -o {} '.format(jobs, logfile)
+        #command = 'python {}/search_algorithm.py {:0.2f} {:0.2f}'.format(simple_dir, ra, dec)
+        #command_queue = batch + command
+        #print(command_queue)
+        ##os.system('./' + command) # Run locally
+        #os.system(command_queue) # Submit to queue
