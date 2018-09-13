43427ac1bb5025f6f5a4b2012e311e6193f215fe
Sidney Mau <sidneymau@gmail.com>
2018-09-12 22:54:56 -0500

    whitespace

--- simple/objects/surveys.py
+++ simple/objects/surveys.py
@@ -29,18 +29,18 @@ class DES(Data):
     Class for working with DES data.
     """
     def __init__(self):
-        self.survey = 'DES'
-        self.nside  = 32
-        self.datadir = '/home/s1/kadrlica/projects/y3a2/data/gold/v2.0/healpix'
-        self.fracdet = 'y3a2_griz_o.4096_t.32768_coverfoot_EQU_decompressed.fits'
-        self.band_1 = 'G'
-        self.band_2 = 'R"'
-        self.mag = 'SOF_PSF_MAG_{}'
-        self.mag_err = 'SOF_PSF_MAG_ERR_{}'
+        self.survey    = 'DES'
+        self.nside     = 32
+        self.datadir   = '/home/s1/kadrlica/projects/y3a2/data/gold/v2.0/healpix'
+        self.fracdet   = 'y3a2_griz_o.4096_t.32768_coverfoot_EQU_decompressed.fits'
+        self.band_1    = 'G'
+        self.band_2    = 'R"'
+        self.mag       = 'SOF_PSF_MAG_{}'
+        self.mag_err   = 'SOF_PSF_MAG_ERR_{}'
         self.mag_dered = 'SOF_PSF_MAG_CORRECTED_{}'
-        self.basis_1 = 'RA'
-        self.basis_2 = 'DEC'
-        self.mag_max = 24.5
+        self.basis_1   = 'RA'
+        self.basis_2   = 'DEC'
+        self.mag_max   = 24.5
 
     def quality_mask(self, data):
         """
