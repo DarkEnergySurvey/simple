773146a3aa5b177c0f4f1242037ffc6b4441677a
Sidney Mau <sidneymau@gmail.com>
2018-09-12 23:27:36 -0500

    Add inits and method for search_by_distance() results

--- simple/objects/results.py
+++ simple/objects/results.py
@@ -29,26 +29,78 @@ class Result:
         self.rdir  = rdir
         self.rfile = rfile
 
-    def write_output(self, ra_peak_array, dec_peak_array, r_peak_array, distance_modulus_array, 
-                    n_obs_peak_array, n_obs_half_peak_array, n_model_peak_array, 
-                    sig_peak_array, mc_source_id_array,  outfile):
-        writer = open(outfile, 'a') # don't overwrite; append
-        for ii in range(0, len(sig_peak_array)):
-            # SIG, RA, DEC, MODULUS, r, n_obs, n_model, mc_source_id
-            writer.write('{:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}\n'.format(sig_peak_array[ii], 
-                                                                                                                             ra_peak_array[ii], 
-                                                                                                                             dec_peak_array[ii], 
-                                                                                                                             distance_modulus_array[ii], 
-                                                                                                                             r_peak_array[ii],
-                                                                                                                             n_obs_peak_array[ii],
-                                                                                                                             n_obs_half_peak_array[ii],
-                                                                                                                             n_model_peak_array[ii],
-                                                                                                                             mc_source_id_array[ii]))
-    
-    def make_list(self):
-       #
+        # Initialize result arrays
+        self.ra_peak_array          = []
+        self.dec_peak_array         = [] 
+        self.r_peak_array           = []
+        self.sig_peak_array         = []
+        self.distance_modulus_array = []
+        self.mc_source_id_array     = []
+        self.n_obs_peak_array       = []
+        self.n_obs_half_peak_array  = []
+        self.n_model_peak_array     = []
+        self.mc_source_id_array     = []
 
-    def read_output(self, results_dir):
-        infiles = sorted(glob.glob(results_dir + '/*.csv'))
-        results = np.concatenate([np.genfromtxt(infile, delimiter=',', names=['SIG', 'RA', 'DEC', 'DISTANCE_MODULUS', 'R_PEAK', 'N_OBS_PEAK', 'N_OBS_HALF_PEAK', 'N_MODEL_PEAK', 'MC_SOURCE_ID']) for infile in infiles])
-        return results
+    def append_results(self, ra_peaks, dec_peaks, r_peaks, sig_peaks, distance_moduli, n_obs_peaks, n_obs_half_peaks, n_model_peaks):
+        """
+        Append, concatenate, and sort results from Search.search_by_distance().
+        """
+        # Append results to result arrays
+        self.ra_peak_array.append(ra_peaks)
+        self.dec_peak_array.append(dec_peaks)
+        self.r_peak_array.append(r_peaks)
+        self.sig_peak_array.append(sig_peaks)
+        self.distance_modulus_array.append(dist_moduli)
+        self.n_obs_peak_array.append(n_obs_peaks)
+        self.n_obs_half_peak_array.append(n_obs_half_peaks)
+        self.n_model_peak_array.append(n_model_peaks)
+        self.mc_source_id_array.append(np.tile(0, len(sig_peaks)))
+
+        # Concatenate result arrays
+        self.ra_peak_array          = np.concatenate(self.ra_peak_array)
+        self.dec_peak_array         = np.concatenate(self.dec_peak_array)
+        self.r_peak_array           = np.concatenate(self.r_peak_array)
+        self.sig_peak_array         = np.concatenate(self.sig_peak_array)
+        self.distance_modulus_array = np.concatenate(self.distance_modulus_array)
+        self.n_obs_peak_array       = np.concatenate(self.n_obs_peak_array)
+        self.n_obs_half_peak_array  = np.concatenate(self.n_obs_half_peak_array)
+        self.n_model_peak_array     = np.concatenate(self.n_model_peak_array)
+        self.mc_source_id_array     = np.concatenate(self.mc_source_id_array)
+
+        # Sort peaks according to significance
+        index_sort                  = np.argsort(self.sig_peak_array)[::-1]
+        self.ra_peak_array          = self.ra_peak_array[index_sort]
+        self.dec_peak_array         = self.dec_peak_array[index_sort]
+        self.r_peak_array           = self.r_peak_array[index_sort]
+        self.sig_peak_array         = self.sig_peak_array[index_sort]
+        self.distance_modulus_array = self.distance_modulus_array[index_sort]
+        self.n_obs_peak_array       = self.n_obs_peak_array[index_sort]
+        self.n_obs_half_peak_array  = self.n_obs_half_peak_array[index_sort]
+        self.n_model_peak_array     = self.n_model_peak_array[index_sort]
+        self.mc_source_id_array     = self.mc_source_id_array[index_sort]
+
+        return
+
+    #def write_output(self, ra_peak_array, dec_peak_array, r_peak_array, distance_modulus_array, 
+    #                n_obs_peak_array, n_obs_half_peak_array, n_model_peak_array, 
+    #                sig_peak_array, mc_source_id_array,  outfile):
+    #    writer = open(outfile, 'a') # don't overwrite; append
+    #    for ii in range(0, len(sig_peak_array)):
+    #        # SIG, RA, DEC, MODULUS, r, n_obs, n_model, mc_source_id
+    #        writer.write('{:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}, {:10.3f}\n'.format(sig_peak_array[ii], 
+    #                                                                                                                         ra_peak_array[ii], 
+    #                                                                                                                         dec_peak_array[ii], 
+    #                                                                                                                         distance_modulus_array[ii], 
+    #                                                                                                                         r_peak_array[ii],
+    #                                                                                                                         n_obs_peak_array[ii],
+    #                                                                                                                         n_obs_half_peak_array[ii],
+    #                                                                                                                         n_model_peak_array[ii],
+    #                                                                                                                         mc_source_id_array[ii]))
+    
+    #def make_list(self):
+    #   #
+
+    #def read_output(self, results_dir):
+    #    infiles = sorted(glob.glob(results_dir + '/*.csv'))
+    #    results = np.concatenate([np.genfromtxt(infile, delimiter=',', names=['SIG', 'RA', 'DEC', 'DISTANCE_MODULUS', 'R_PEAK', 'N_OBS_PEAK', 'N_OBS_HALF_PEAK', 'N_MODEL_PEAK', 'MC_SOURCE_ID']) for infile in infiles])
+    #    return results
